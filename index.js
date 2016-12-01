fc8db6a21876cf38a381da0486a7d7504a318708
Mozartted <mozart.osita@gmail.com>
2016-12-01 21:07:10 +0100

    Creating node sections

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 //the server components.
 var express=require("express");
-var Mongoose=require("mongoose");
+var mongoose=require("mongoose");
 var path = require('path');
 var request = require('request');
+var config= require('./config')
+
+var Task=mongoose.model('Task',new mongoose.Schema(
+    {
+        'id':{'type':String},
+        'task':{type:String},
+        'task_status':Boolean
+    }
+))
+
+mongoose.connect(config.db);
+
+var app=express();
+
+app.set('port',process.env.PORT||3000);
+app.use(express.static(path.join(__dirname,'app')));
+
+//setting up express values
