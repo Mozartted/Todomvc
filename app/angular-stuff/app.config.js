cbecbf6141e43ac23c637c59625120b73323a905
Mozartted <mozart.osita@gmail.com>
2016-12-06 05:57:18 +0100

    Adjusting module to render view without ng-area error

---
+++ app/angular-stuff/app.config.js
@@ -0,0 +1,24 @@
+app.config(['$routeProvider',function($routeProvider){
+    'use strict';
+
+    var routeConfig={
+            'controller':'TodoController',
+            'templateUrl':'angular-stuff/html/todo.html',
+            // resolve: {
+			// 	store: function (todoStorage) {
+			// 		// Get the correct module (API or localStorage).
+			// 		return todoStorage.then(function (module) {
+			// 			module.get(); // Fetch the todo records in the background.
+			// 			return module;
+			// 		});
+			// 	}
+			// }
+    };
+
+    $routeProvider
+    .when('/',routeConfig)
+    .otherwise({
+        redirectTo:'/'
+    });
+
+}]);
--- app/angular-stuff/app.module.js
+++ app/angular-stuff/app.module.js
@@ -1,25 +1,3 @@
-angular.module('TodoMVC',['ngRoute','ngResource'])
-.config(['$routeProvider'],function(){
-    'use strict';
+angular.module('TodoMVC',['ngRoute','ngResource']);
 
-    var routeConfig={
-            'controller':'TodoController',
-            'templateUrl':'angular-stuff/html/todo.html',
-            resolve: {
-				store: function (todoStorage) {
-					// Get the correct module (API or localStorage).
-					return todoStorage.then(function (module) {
-						module.get(); // Fetch the todo records in the background.
-						return module;
-					});
-				}
-			}
-    };
-
-    $routeProvider
-    .when('/',routeConfig)
-    .otherwise({
-        redirectTo:'/'
-    });
-
-});
+var app=angular.module('TodoMVC');
