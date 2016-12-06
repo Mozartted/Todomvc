60cf81f5b93cc701ef4be6a74e609ef2a7076cca
Mozartted <mozart.osita@gmail.com>
2016-12-06 06:42:29 +0100

    Re-amending controller to views, todo scope

--- app/angular-stuff/controllers/TodoController.js
+++ app/angular-stuff/controllers/TodoController.js
@@ -1,22 +1,31 @@
-app.controller('TodoController',function TodoController ($ctrl,$routeParams,$filter){
+app.controller('TodoController',function TodoController ($routeParams,$filter,store){
         //the todo controller would be handling new additions to the
         //todo list and applying these to the DB via node and $resource
 
         'use strict';
+        var self=this;
 
-        $ctrl.newTask = ' ';
+        var todos = self.todos = store.todos;
 
-        $ctrl.addTodo = function()
+        self.newTask = ' ';
+
+        self.addTodo = function(task)
         {
             var newTask={
                 title:$ctrl.newTask.trim(),
                 completed:false
             }
+
+            store.insert(newTask)
+            .then(function success() {
+                self.newTodo = '';
+            })
+            .finally(function () {
+                self.saving = false;
+            });
+
         }
 
-        if(!newTask.title){
-            return;
-        }
 
 
 
