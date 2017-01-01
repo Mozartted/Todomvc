934e6af8fddc068e694f96828a49697c590510ab
Mozartted <mozart.osita@gmail.com>
2017-01-01 20:17:02 +0100

    Enabling Mongo sb Promises

--- server/config/createdb.js
+++ server/config/createdb.js
@@ -2,6 +2,7 @@ var mongoose   = require('mongoose'),
     secrets= require('./secrets');
 
 var db = mongoose.connection;
+mongoose.Promise = global.Promise;
 mongoose.connect(secrets.db);
 
 module.exports = {
