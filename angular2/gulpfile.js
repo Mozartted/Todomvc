eceac7b5386fde216373ddfe5ac2d02a4e62b22d
Mozartted <mozart.osita@gmail.com>
2017-01-07 20:40:32 +0100

    base for gulp file

---
+++ angular2/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp          =   require("gulp"),
+    browserSync   =   require("browser-sync"),
+    concat        =   require("gulp-concat"),
+    uglify        =   require("gulp-uglify");
+
+gulp.task("angularStuff",function(){
+    return gulp.src(
+      [
+
+      ]
+    ).pipe();
+
+
+});
