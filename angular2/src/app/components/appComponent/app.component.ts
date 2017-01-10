a2cb4917115d25f44c45f16106401ecf339d6342
Mozartted <mozart.osita@gmail.com>
2017-01-10 20:06:46 +0100

    Moving again

---
+++ angular2/src/app/components/appComponent/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {TodoStoreService} from './service/TodoStore/todo-store.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './components/appComponent/app.component.html',
+  styleUrls: ['./components/appComponent/app.component.css'],
+  provider:[]
+})
+export class AppComponent {
+  contructor(){
+
+  }
+
+}
--- angular2/src/app/service/TodoStore/todo-store.service.ts
+++ angular2/src/app/service/TodoStore/todo-store.service.ts
@@ -1,8 +1,65 @@
 import { Injectable } from '@angular/core';
 
+import { Todo } from './service/todo';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
 @Injectable()
 export class TodoStoreService {
+  todos:Array<Todo>;
 
-  constructor() { }
+  constructor(private http:Http) {
+    let collectedTodos=
+
+  }
+
+}
+
+@Injectable()
+class api{
+  todos:Array<Todo>;
+
+  constructor(private http:Http) {
+    let collectedTodos=http.get()
+
+  }
+}
+
+@Injectable()
+class api{
+  todos:Array<Todo>;
+
+  private http;
+
+  constructor(private http:Http) {
+    let collectedTodos=api.get('api/')
+                        .toPromise()
+                        .then(response => response.json().data as Todo[]);
+    todos=collectedTodos;
+
+  }
+
+  api:http;
+
+
+  add():Promise<Todo>{
+
+  }
+
+  delete(){
+
+  }
+
+  retrieve():Promise<Todo>{
+
+  }
+
+  allCompleted(){
+
+  }
+
+  getRemaining(){
+
+  }
 
 }
