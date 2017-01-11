19eace5a940252a22586ef3d8b4c47833e043bb9
Mozartted <mozart.osita@gmail.com>
2017-01-11 18:20:46 +0100

    Adding Observable, time to get response like a big boy :smile:

--- angular2/client/src/app/service/TodoStore/todo-store.service.ts
+++ angular2/client/src/app/service/TodoStore/todo-store.service.ts
@@ -3,30 +3,15 @@ import { Injectable } from '@angular/core';
 import { Todo } from './service/todo';
 import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
+import {Observable} from 'rxjs/Rx';
+
+// Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class TodoStoreService {
-  todos:Array<Todo>;
 
-  constructor(private http:Http) {
-    let collectedTodos=
-
-  }
-
-}
-
-@Injectable()
-class api{
-  todos:Array<Todo>;
-
-  constructor(private http:Http) {
-    let collectedTodos=http.get()
-
-  }
-}
-
-@Injectable()
-class api{
   todos:Array<Todo>;
 
   private http;
