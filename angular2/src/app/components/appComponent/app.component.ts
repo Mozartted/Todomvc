fa470535f67093cd4ac9627445ac21f5f803e1ac
Mozartted <mozart.osita@gmail.com>
2017-01-11 14:14:39 +0100

    Modified Component

--- angular2/src/app/components/appComponent/app.component.ts
+++ angular2/src/app/components/appComponent/app.component.ts
@@ -5,11 +5,43 @@ import {TodoStoreService} from './service/TodoStore/todo-store.service';
   selector: 'app-root',
   templateUrl: './components/appComponent/app.component.html',
   styleUrls: ['./components/appComponent/app.component.css'],
-  provider:[]
+  provider:[TodoStoreService]
 })
 export class AppComponent {
-  contructor(){
+  todoStore: TodoStoreService;
+  newTodoText = '';
+
+  contructor(todoStore: TodoStoreService){
+    this.todoStore=todoStore;
+  }
+
+  addTodo():{
 
   }
+
+  stopEditing(todo: Todo, editedTitle: string) {
+		todo.title = editedTitle;
+		todo.editing = false;
+	}
+
+	cancelEditingTodo(todo: Todo) {
+		todo.editing = false;
+	}
+
+	updateEditingTodo(todo: Todo, editedTitle: string) {
+		editedTitle = editedTitle.trim();
+		todo.editing = false;
+
+		if (editedTitle.length === 0) {
+			return this.todoStore.remove(todo);
+		}
+
+		todo.title = editedTitle;
+	}
+
+  removeCompleted():{
+
+  }
+
 
 }
