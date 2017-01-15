0046a7a651ea75711f19a2934c1f358cf7e273a5
Mozartted <mozart.osita@gmail.com>
2017-01-15 20:20:23 +0100

    Created post service

--- angular2/client/src/app/service/TodoStore/todo-store.service.ts
+++ angular2/client/src/app/service/TodoStore/todo-store.service.ts
@@ -12,17 +12,32 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class TodoStoreService {
 
-  todos:Array<Todo>;
-  baseUrl='http://localhost:3000/api/';
+
   //am not really sure but I feel this is where the cool data would go.
 
-  constructor(private http:Http) {}
+  constructor(private http:Http,) {}
+  public todos:Array<Todo>;
+  public baseUrl='http://localhost:3000/api/';
 
-  api=this.http;
+  add(todo:Object):Observable<Todo[]>{
+    let todoString=JSON.stringify(todo); //creating String payload
+    let headers = new Headers({'Content-Type':'application/json'}); //set content type to json
+    let options = new RequestOptions({headers: headers}); //creates a request option
 
-  add(todo:Todo):Observable<Todo[]>{
-
+    return this.http.post(baseurl,todo,options)
+              .map((res:Response)=>res.json())
+              .catch((error:any)=>Observable.throw(error.json().error || 'Server error'));
   }
+
+  // addComment (body: Object): Observable<Comment[]> {
+  //     let bodyString = JSON.stringify(body); // Stringify payload
+  //     let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+  //     let options = new RequestOptions({ headers: headers }); // Create a request option
+  //
+  //     return this.http.post(this.commentsUrl, body, options) // ...using post request
+  //                      .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
+  //                      .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+  // }
 
   delete(){
 
