24732f193f812d248e6364278935f63e36c69a52
Mozartted <mozart.osita@gmail.com>
2017-01-31 05:57:34 +0100

    Added gulpfile to angular frame

---
+++ angular2/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp      =    require('gulp'),
+    concat    =    require('gulp-concat'),
+    uglify    =    require('gulp-uglify');
+
+
+gulp.task('todoscripts',function(){
+    return gulp.src(
+             [
+                 'node_modules/todomvc-common/base.css',
+                 'node_modules/todomvc-app-css/index.css'
+             ]
+           )
+           .pipe(concat('styles.css'))
+           .pipe(gulp.dest('client/src'));
+});
+
+gulp.task('default',['todoscripts']);  
