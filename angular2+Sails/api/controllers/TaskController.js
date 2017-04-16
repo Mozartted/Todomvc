6527b372bf8afaa68d8095b3c07b2f4e633e1a2c
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2017-04-16 13:18:12 +0100

    Fill in functions for Controller Operations

--- angular2+Sails/api/controllers/TaskController.js
+++ angular2+Sails/api/controllers/TaskController.js
@@ -6,24 +6,36 @@
  */
 
 module.exports = {
-	create:function(res,req){
-
+	create:function(req,res){
+        var Todo = (req.body);
+        TaskService.create(Todo,function(err,tasks){
+            if(err) return res.json(err)
+            return res.json(tasks);
+        });
     },
 
-    retrieveAll:function(res,req){
-
-    },
-
-    retrieve:function(res,req){
-
+    retrieveAll:function(req,res){
+        TaskService.get(function(todos){
+            res.json(todos);
+        });
     },
 
     update:function(res,req){
-
+        var task_id = res.params.id;
+        TaskService.get(task_id,function(todo){
+            todo.update(res.body).exec(function(err,todo){
+                return res.json({status:'success',message:'task updated'})
+            });
+        });
     },
 
     delete:function(res,req){
-
+        var task_id = res.params.id;
+        TaskService.delete(task_id,function(status){
+            if(status === 'success'){
+                return res.json({status:'success',message:'deleted task'})
+            }
+        });
     }
 };
 
