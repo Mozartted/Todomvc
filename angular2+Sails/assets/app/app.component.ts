ab538f23e5964e49be75dc563aa9320b2eefaed2
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2017-04-17 19:41:23 +0100

    Base component

--- angular2+Sails/assets/app/app.component.ts
+++ angular2+Sails/assets/app/app.component.ts
@@ -1,6 +1,39 @@
 import {Component} from '@angular/core';
+import {TaskService} from './task.service';
+import {Task} from './task';
+
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular/Sails App</h1>'
 })
-export class AppComponent { }
+export class AppComponent { 
+
+    todoText:String;
+    taskStore: TaskService
+
+    constructor(taskStore:TaskService){
+        // create a list of tasks
+        this.taskStore = taskStore;
+    }
+
+    addTask(){
+        // adding the new task
+        let task = new Task(this.todoText);
+        this.taskStore.addTask();
+    }
+
+    removeTask(task : Task){
+        // remove the given task
+        this.taskStore.remove();
+    }
+
+    editTask(task: Task, editedTask: string){
+        // edit the given task
+        // check that editedTask equal actual values
+        if(editedTask!==null || editedTask!==''){
+            task.tasks=editedTask;
+            this.taskStore.updateTask();
+        }
+    }
+
+}
