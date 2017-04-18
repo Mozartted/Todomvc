b7ee30ded14055ef317ecc2778f972c2f47d50fc
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2017-04-19 00:08:37 +0100

    Setting up and task service

--- angular2+Sails/assets/app/task.service.ts
+++ angular2+Sails/assets/app/task.service.ts
@@ -16,11 +16,37 @@ export class TaskService{
               .map((res:Response)=>res.json())
               .catch((error:any)=>Observable.throw(error.json().error || 'Server error'));
 
-        tasks.subscribe( 
+        tasks.subscribe(
             tasks => this.task = tasks, //Bind to view
             err => {
             console.log(err);
         });
     }
 
+    updateTask(task:Task):Observable<Task[]>{
+      // take the task variable and update it with the request
+      return this.http.post(`${this.baseUrl}${task.id}`,task)
+                  .map((res:Response)=>{
+                      res.json()
+                  })
+                  .catch((err:any)=>Observable.throw(err.json().error || 'Server error'));
+    }
+
+    addTask(task:Task):Observable<Task[]>{
+      return this.http.post(`${this.baseUrl}`,task)
+                  .map((res:Response)=>{
+                      res.json()
+                  })
+                  .catch((err:any)=>Observable.throw(err.json().error || 'Server error'));
+
+    }
+
+    remove(task:Task):Observable<Task[]>{
+      return this.http.delete(`${this.baseUrl}${task.id}`)
+                  .map((res:Response)=>{
+                      res.json()
+                  })
+                  .catch((err:any)=>Observable.throw(err.json().error || 'Server error'));
+
+    }
 }
